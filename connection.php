d2b8b628ebdd60c39f4147206d6769cb13ea8188
Aloka_Chamod_Champathi <145195460+Alokachamod@users.noreply.github.com>
2024-05-21 15:55:30 +0530

    #

--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@ class Database {
 
     public static function setUpConnection(){
         if(!isset(Database::$connection)){
-            Database::$connection = new mysqli("localhost","root","1234","eshop","3306");
+            Database::$connection = new mysqli("localhost","root","Shakuloka@.1","eshop","3306");
         }
     }
 
